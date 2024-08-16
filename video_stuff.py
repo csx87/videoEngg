c998aa8d0f01edef1bdb3df0f612021a9cb8f68d
chaman.suresh <chaman.sureshbabu@gmail.com>
2024-08-16 22:58:33 +0530

    Getting frame rate from video meta data

--- video_stuff.py
+++ video_stuff.py
@@ -12,15 +12,16 @@ class VideoFile:
         self.path = name
         self.width = 0
         self.height = 0
+        self.frame_rate = 0
         self.aspect_ratio = Fraction(0, 1)
         self.isHDR = False #TODO: Get the info from probe 
         self.__initialize()
 
     def __initialize(self):
         try:
-            self.width, self.height, self.aspect_ratio,self.isHDR = self.__get_metadata_from_video()
+            self.width, self.height, self.aspect_ratio,self.frame_rate, self.isHDR = self.__get_metadata_from_video()
         except Exception as e:
-            print(f"Failed to initialize video resolution: {e}")
+            print(f"Failed to initialize video resolution: {e}","Pleas check if the video file is proper")
             # Optional: Set to default values or handle error as needed
             self.width, self.height, self.aspect_ratio = 0, 0, Fraction(0, 1)
 
@@ -29,17 +30,23 @@ class VideoFile:
         height = 0
         aspect_ratio = Fraction(0, 1) 
         isHDR = False
+        frame_rate = 0
 
         try:
-            probe = ffmpeg.probe(self.path, v='error', select_streams='v:0', show_entries='stream=height,width,color_space,color_transfer,color_primaries')
+            probe = ffmpeg.probe(
+                self.path, v='error', 
+                select_streams='v:0', 
+                show_entries='stream=height,width,color_space,color_transfer,color_primaries,r_frame_rate',
+                )
+
             if "streams" in probe.keys():
-               
                 stream_data = probe["streams"][0]
-                
-                if "width" in stream_data and "height" in stream_data:
+                print(stream_data) 
+                if "width" in stream_data and "height" in stream_data and "r_frame_rate" in stream_data:
                     width = stream_data["width"]
                     height = stream_data["height"]
                     aspect_ratio = Fraction(width, height)
+                    frame_rate = eval(stream_data["r_frame_rate"])
                 if "color_space" in stream_data and "color_transfer" in stream_data and "color_primaries" in stream_data:
                     if stream_data["color_primaries"] == 'bt2020' and stream_data["color_transfer"] in ['smpte2084', 'arib-std-b67'] and stream_data["color_space"] == 'bt2020nc':
                         isHDR = True
@@ -56,7 +63,7 @@ class VideoFile:
             # Raising an exception to be handled by the calling method
             raise
 
-        return width, height, aspect_ratio, isHDR
+        return width, height, aspect_ratio,frame_rate, isHDR, 
 
 
 
@@ -93,8 +100,7 @@ def transcode_to_h265_and_insert_a_circle(videoFile : VideoFile,output_height: i
             inv = ffmpeg.input(videoFile.path)
             ini = ffmpeg.input(circle_path)
             output_path = f"{TEMP_DIR}/output_{output_height}.mp4"
-            #TODO get frames from videoFile
-            segment_size = int((segement_duration_op/1000)*24)
+            segment_size = int((segement_duration_op/1000)*videoFile.frame_rate)
             print("Segment size:",segment_size)
 
             ffmpeg.output(inv,ini,
@@ -156,16 +162,16 @@ def transcode_to_h265_and_insert_a_circle(videoFile : VideoFile,output_height: i
 if __name__ == "__main__":
 
     start_time = time.time()
-    video = VideoFile('./input_hdr.mkv')
+    video = VideoFile('./input.mp4')
     #video = VideoFile('./tmp/output_720.mp4')
-    print(f"Width: {video.width}, Height: {video.height}, Aspect Ratio: {video.aspect_ratio} ,HDR video:{'yes' if video.isHDR else 'no'}")
+    print(f"Width: {video.width}, Height: {video.height}, Aspect Ratio: {video.aspect_ratio}, frame_rate:{round(video.frame_rate,2)} ,HDR video:{'yes' if video.isHDR else 'no'}")
 
-    video_720,video_720_sdr = transcode_to_h265_and_insert_a_circle(video,360)
+    video_720 = transcode_to_h265_and_insert_a_circle(video,360)
 
 
     end_time = time.time()
 
-    print(f"Total Time take = {round(end_time -start_time)} sec", video_720.path,video_720_sdr.path)
+    print(f"Total Time take = {round(end_time -start_time)} sec", video_720.path)
 
 
 
