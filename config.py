7c8f645bcebe0e8f8477c672a12d4f9e9e5aedde
chaman.suresh <chaman.sureshbabu@gmail.com>
2024-08-17 02:17:32 +0530

    Setting the proper bento4 sdk path

--- config.py
+++ config.py
@@ -1,6 +1,19 @@
-#---------------TRANSCODING_PARAMETERS-------------------------------------------#
+import sys
+import os
+
+
 CONSTANT_RATE_FACTOR = 28
 PRESET = "medium"
-SEGMENT_DURATION = 7500 # 7.5 sec
-HDR2SDR_FILTER = f"zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=0,zscale=t=bt709:m=bt709:r=tv,format=yuv420p[scaled];"
-BENTO4_SDK_PATH = "/home/csx87/lib/Bento4-SDK-1-6-0-641.x86_64-unknown-linux"
+SEGMENT_DURATION = 7500  # 7.5 sec
+HDR2SDR_FILTER = "zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=0,zscale=t=bt709:m=bt709:r=tv,format=yuv420p[scaled];"
+BENTO4_SDK_MACOS = "Bento4-SDK-1-6-0-641.universal-apple-macosx"
+BENTO4_SDK_WIN = "Bento4-SDK-1-6-0-641.x86_64-microsoft-win32"
+BENTO4_SDK_LINUX = "Bento4-SDK-1-6-0-641.x86_64-unknown-linux"
+
+if os.name == 'nt':  # 'nt' for Windows
+    BENTO4_SDK_PATH = BENTO4_SDK_WIN
+elif sys.platform == 'darwin':  # macOS
+    BENTO4_SDK_PATH = BENTO4_SDK_MACOS
+else:  # Assume Linux
+    BENTO4_SDK_PATH = BENTO4_SDK_LINUX
+
